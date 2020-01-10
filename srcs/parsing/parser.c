7f8ef74d302c60a22d7803fee6467b43b2b5e85d
Martin CLAUDEL <mclaudel@e1r4p2.42.fr>
2020-01-10 15:16:20 +0100

    parsing irst version

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,13 +6,11 @@
 /*   By: mclaudel <mclaudel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 13:09:56 by mclaudel          #+#    #+#             */
-/*   Updated: 2020/01/10 15:12:28 by mclaudel         ###   ########.fr       */
+/*   Updated: 2020/01/10 15:15:45 by mclaudel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <parser.h>
-
-#include <stdio.h>
 
 int		parse_world(t_minirt *rt, char *path)
 {
@@ -132,75 +130,4 @@ int		parsing_error(char *msg, int n)
 	}
 	write(1, "\n\e[0m", 5);
 	return (ERROR);
-}
-
-int		parse_vec3(t_vec3 *v, char *line)
-{
-	int		rd;
-	int		r;
-	int		i;
-	double	*p;
-
-	rd = 0;
-	i = 3;
-	p = &v->x;
-	while (i--)
-	{
-		if ((r = parse_double(p, line)) == ERROR)
-			return (ERROR);
-		rd += r;
-		line += r;
-		if (i != 0 && *line++ != ',')
-			return (ERROR);
-		if (i != 0)
-			rd++;
-		p++;
-	}
-	return (rd);
-}
-
-int		parse_color(unsigned int *c, char *line)
-{
-	t_vec3	v;
-	t_color color;
-	int		r;
-
-	if ((r = parse_vec3(&v, line)) == ERROR || !v3drange(v, 0, 255))
-		return (ERROR);
-	color.color.r = v.x;
-	color.color.g = v.y;
-	color.color.b = v.z;
-	*c = color.v;
-	return (r);
-}
-
-// Check for double size
-int		parse_double(double *v, char *line)
-{
-	double	a;
-	double 	coef;
-	int		read;
-	double	sign;
-
-	a = 0;
-	read = 0;
-	coef = 1;
-	sign = 1;
-	while ((ft_isspace (*line) || *line == '-' || *line == '+') && ++read)
-		if (*line++ == '-')
-			sign *= -1;
-	if (!ft_isdigit(*line))
-		return (ERROR);
-	while (ft_isdigit(*line) && ++read)
-		a = a * 10 + *(line++) - '0';
-	if (*line++ == '.' && ++read)
-		while (ft_isdigit(*line))
-		{
-			coef /= 10;
-			a += (double)((*(line++) - '0') * coef);
-			read++;
-		}
-	// if (*[line - 1] )
-	*v = a * sign;
-	return (read);
 }
---
+++ srcs/parsing/utilsparsers.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utilsparsers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mclaudel <mclaudel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/10 15:15:36 by mclaudel          #+#    #+#             */
+/*   Updated: 2020/01/10 15:15:50 by mclaudel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <parser.h>
+
+int		parse_vec3(t_vec3 *v, char *line)
+{
+	int		rd;
+	int		r;
+	int		i;
+	double	*p;
+
+	rd = 0;
+	i = 3;
+	p = &v->x;
+	while (i--)
+	{
+		if ((r = parse_double(p, line)) == ERROR)
+			return (ERROR);
+		rd += r;
+		line += r;
+		if (i != 0 && *line++ != ',')
+			return (ERROR);
+		if (i != 0)
+			rd++;
+		p++;
+	}
+	return (rd);
+}
+
+int		parse_color(unsigned int *c, char *line)
+{
+	t_vec3	v;
+	t_color color;
+	int		r;
+
+	if ((r = parse_vec3(&v, line)) == ERROR || !v3drange(v, 0, 255))
+		return (ERROR);
+	color.color.r = v.x;
+	color.color.g = v.y;
+	color.color.b = v.z;
+	*c = color.v;
+	return (r);
+}
+
+// Check for double size
+int		parse_double(double *v, char *line)
+{
+	double	a;
+	double 	coef;
+	int		read;
+	double	sign;
+
+	a = 0;
+	read = 0;
+	coef = 1;
+	sign = 1;
+	while ((ft_isspace (*line) || *line == '-' || *line == '+') && ++read)
+		if (*line++ == '-')
+			sign *= -1;
+	if (!ft_isdigit(*line))
+		return (ERROR);
+	while (ft_isdigit(*line) && ++read)
+		a = a * 10 + *(line++) - '0';
+	if (*line++ == '.' && ++read)
+		while (ft_isdigit(*line))
+		{
+			coef /= 10;
+			a += (double)((*(line++) - '0') * coef);
+			read++;
+		}
+	// if (*[line - 1] )
+	*v = a * sign;
+	return (read);
+}
